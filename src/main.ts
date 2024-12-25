8c5fbead300ab7cdedc4fc559759f9ee85d6a53d
xhowaisshah <Xhowais@gmail.com>
2024-12-26 02:34:30 +0500

    Update CORS configuration to allow requests from any origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: 'X-Requested-With,Content-Type,Authorization',
   });
